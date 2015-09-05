5c761ed33f20c6c07c08cc7eaaddbdc356cb2565
Andrew Khaynus <a.khaynus@gmail.com>
2015-09-05 12:18:20 +0300

    create SomeClass

---
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/widget/SomeClass.java
@@ -0,0 +1,10 @@
+package com.example.d_rom.supportdesigndemo.widget;
+
+/**
+ * Created by Radomar on 05.09.2015.
+ */
+public class SomeClass {
+    private int mInteger;
+    private String mString;
+
+}
